82406f4bbe6074873c89f64d081bc67f8147be87
Andrei Mihalescu <andreimihalescu07@gmail.com>
2022-05-24 23:21:22 +0300

    BP-1616 Giropay payment method

---
+++ src/PaymentMethods/Giropay.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Buckaroo\PaymentMethods;
+
+use Buckaroo\Model\PaymentPayload;
+use Buckaroo\Model\ServiceList;
+
+class Giropay extends PaymentMethod
+{
+    public const SERVICE_VERSION = 2;
+
+    public function setPayServiceList(array $serviceParameters = []): self
+    {
+        $paymentModel = new PaymentPayload($this->payload);
+
+        $parameters = array([
+            'name' => 'bic',
+            'Value' => $paymentModel->bic
+        ]);
+
+        $serviceList = new ServiceList(
+            self::GIROPAY,
+            self::SERVICE_VERSION,
+            'Pay',
+            $parameters
+        );
+
+        $this->request->getServices()->pushServiceList($serviceList);
+
+        return $this;
+    }
+
+    public function setRefundServiceList(): self
+    {
+        $serviceList =  new ServiceList(
+            self::GIROPAY,
+            self::SERVICE_VERSION,
+            'Refund'
+        );
+
+        $this->request->getServices()->pushServiceList($serviceList);
+
+        return $this;
+    }
+}
