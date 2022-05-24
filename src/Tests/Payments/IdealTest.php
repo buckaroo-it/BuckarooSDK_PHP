57c25ec3cf4a7f36ab41ef12a3de933847dde35e
Shu Chen <s.chen@buckaroo.nl>
2022-05-24 06:58:12 +0200

    Adding unit test

--- src/Tests/Payments/IdealTest.php
+++ src/Tests/Payments/IdealTest.php
@@ -2,23 +2,43 @@
 
 namespace Buckaroo\Tests\Payments;
 
-use Buckaroo\Buckaroo;
-use PHPUnit\Framework\TestCase;
+use Buckaroo\Tests\BuckarooTestCase;
 
-class IdealTest extends TestCase
+class IdealTest extends BuckarooTestCase
 {
     protected function setUp(): void
     {
-        $this->buckaroo = new Buckaroo($_ENV['BPE_WEBSITE_KEY'], $_ENV['BPE_SECRET_KEY']);
-    }
-
-    public function test_it_creates_a_ideal_payment()
-    {
-        $response = $this->buckaroo->payment('idealprocessing')->pay([
+        $this->paymentPayload = ([
             'issuer' => 'ABNANL2A',
             'amountDebit' => 10.10
         ]);
 
+        $this->refundPayload = [
+            'invoice'   => '', //Set invoice number of the transaction to refund
+            'originalTransactionKey' => '', //Set transaction key of the transaction to refund
+            'amountCredit' => 10.10
+        ];
+    }
+
+    /**
+     * @return void
+     * @test
+     */
+    public function it_creates_a_ideal_payment()
+    {
+        $response = $this->buckaroo->payment('idealprocessing')->pay($this->paymentPayload);
         $this->assertTrue($response->isPendingProcessing());
+
+        $response = $this->buckaroo->payment('ideal')->pay(json_encode($this->paymentPayload));
+        $this->assertTrue($response->isPendingProcessing());
+    }
+
+    /**
+     * @test
+     */
+    public function it_creates_a_ideal_refund()
+    {
+        $response = $this->buckaroo->payment('ideal')->refund($this->refundPayload);
+        $this->assertTrue($response->isSuccess());
     }
 }
