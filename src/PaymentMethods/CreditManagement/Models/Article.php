eca244ba2eba35fd440b93a51b8635de1ebfb6c5
Shu Chen <s.chen@buckaroo.nl>
2022-06-21 07:18:33 +0200

    Credit Management Product Lines

--- src/PaymentMethods/CreditManagement/Models/Article.php
+++ src/PaymentMethods/CreditManagement/Models/Article.php
@@ -4,6 +4,14 @@ namespace Buckaroo\PaymentMethods\CreditManagement\Models;
 
 class Article extends \Buckaroo\Models\Article
 {
+    protected string $productLine;
+    protected string $productGroupName;
+    protected string $productGroupOrderIndex;
+    protected string $productOrderIndex;
+    protected string $unitOfMeasurement;
+    protected float $discountPercentage;
+    protected float $totalDiscount;
     protected float $totalVat;
+    protected float $totalAmountExVat;
     protected float $totalAmount;
 }
