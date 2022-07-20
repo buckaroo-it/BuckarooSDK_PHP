50e84508f955a8c85cd2b2e4bce7c6eb9c2ec771
Shu Chen <s.chen@buckaroo.nl>
2022-07-20 05:32:19 +0200

    changing the method to constructor

--- src/Handlers/Reply/Json.php
+++ src/Handlers/Reply/Json.php
@@ -11,19 +11,21 @@ class Json implements ReplyStrategy
     private array $data;
     private string $auth_header;
     private string $uri;
+    private string $method;
 
-    public function __construct(Config $config, array $data, string $auth_header = '', string $uri = '')
+    public function __construct(Config $config, array $data, string $auth_header = '', string $uri = '', $method = 'POST')
     {
         $this->config = $config;
         $this->data = $data;
         $this->auth_header = $auth_header;
         $this->uri = $uri;
+        $this->method = $method;
     }
 
     public function validate(): bool
     {
         $validator = new Validator($this->config);
 
-        return $validator->validate($this->auth_header, $this->uri, 'POST', $this->data);
+        return $validator->validate($this->auth_header, $this->uri, $this->method, $this->data);
     }
 }
