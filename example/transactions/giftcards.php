931e36a522901a71292177019e2ad27eeb95d2cf
Andrei Mihalescu <andrei.mihalescu@arnia.ro>
2022-09-22 17:22:55 +0300

    Giftcard redirect method + example fixes (#41)
    * Giftcard redirect method + example fixes
    
    * Giftcard review update

--- example/transactions/giftcards.php
+++ example/transactions/giftcards.php
@@ -8,16 +8,13 @@ $buckaroo = new BuckarooClient($_ENV['BPE_WEBSITE_KEY'], $_ENV['BPE_SECRET_KEY']
 
 //Also accepts json
 //Pay
-$response = $buckaroo->method('giftcard')->pay([
+$response = $buckaroo->method('boekenbon')->pay([
     'amountDebit' => 10,
     'invoice' => uniqid(),
-    'serviceParameters' => [
-        'name'          => 'boekenbon',
-        'voucher'      => [
-            'intersolveCardnumber' => '0000000000000000001',
-            'intersolvePin'        => '1000'
-        ]
-    ]
+     'name' => 'boekenbon',
+     'intersolveCardnumber' => '0000000000000000001',
+     'intersolvePIN'        => '1000'
+    
 ]);
 
 //Refund
@@ -25,7 +22,5 @@ $response = $buckaroo->method('giftcard')->refund([
     'amountCredit' => 10,
     'invoice'       => 'testinvoice 123',
     'originalTransactionKey' => '2D04704995B74D679AACC59F87XXXXXX',
-    'serviceParameters' => [
-        'name'          => 'boekenbon'
-    ]
+    'name'          => 'boekenbon'
 ]);
