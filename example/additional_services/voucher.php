9c92ebdfe2688113be881d54186dabc5f8415ccb
Ivascu Madalin <madalin.ivascu@arnia.ro>
2022-10-19 17:17:44 +0300

    Buckaroo voucher

---
+++ example/additional_services/voucher.php
@@ -0,0 +1,49 @@
+<?php
+
+require('../bootstrap.php');
+
+use Buckaroo\BuckarooClient;
+
+$buckaroo = new BuckarooClient($_ENV['BPE_WEBSITE_KEY'], $_ENV['BPE_SECRET_KEY']);
+
+
+//Create voucher
+$response = $buckaroo->method('buckaroovoucher')->create(
+    [
+        'usageType' => '2',
+        'validFrom' => '2022-01-01',
+        'validUntil' => '2024-01-01',
+        'creationBalance' => '5',
+    ]
+);
+
+//Pay
+$response = $buckaroo->method('buckaroovoucher')->pay(
+    [
+        'vouchercode' => 'vouchercode'
+    ]
+);
+
+
+//Refund
+$response = $buckaroo->method('buckaroovoucher')->refund(
+    [
+        'amountCredit' => 10,
+        'invoice'       => 'testinvoice 123',
+        'originalTransactionKey' => '4E8BD922192746C3918BF4077CXXXXXX'
+    ]
+);
+
+//Get Balance
+$response = $buckaroo->method('buckaroovoucher')->getBalance(
+    [
+        'vouchercode' => 'vouchercode'
+    ]
+);
+
+//Deactivate
+$response = $buckaroo->method('buckaroovoucher')->deactivate(
+    [
+        'vouchercode' => 'vouchercode'
+    ]
+);
--- src/PaymentMethods/PaymentMethodFactory.php
+++ src/PaymentMethods/PaymentMethodFactory.php
@@ -20,46 +20,47 @@
 
 namespace Buckaroo\PaymentMethods;
 
-use Buckaroo\Exceptions\BuckarooException;
-use Buckaroo\Exceptions\SDKException;
-use Buckaroo\PaymentMethods\Afterpay\Afterpay;
-use Buckaroo\PaymentMethods\AfterpayDigiAccept\AfterpayDigiAccept;
-use Buckaroo\PaymentMethods\Alipay\Alipay;
-use Buckaroo\PaymentMethods\ApplePay\ApplePay;
-use Buckaroo\PaymentMethods\Bancontact\Bancontact;
-use Buckaroo\PaymentMethods\Belfius\Belfius;
-use Buckaroo\PaymentMethods\Billink\Billink;
-use Buckaroo\PaymentMethods\BuckarooWallet\BuckarooWallet;
-use Buckaroo\PaymentMethods\CreditCard\CreditCard;
-use Buckaroo\PaymentMethods\CreditClick\CreditClick;
-use Buckaroo\PaymentMethods\CreditManagement\CreditManagement;
-use Buckaroo\PaymentMethods\Emandates\Emandates;
+use Buckaroo\Transaction\Client;
 use Buckaroo\PaymentMethods\EPS\EPS;
-use Buckaroo\PaymentMethods\GiftCard\GiftCard;
-use Buckaroo\PaymentMethods\Giropay\Giropay;
-use Buckaroo\PaymentMethods\iDeal\iDeal;
-use Buckaroo\PaymentMethods\iDealQR\iDealQR;
-use Buckaroo\PaymentMethods\iDin\iDin;
 use Buckaroo\PaymentMethods\In3\In3;
 use Buckaroo\PaymentMethods\KBC\KBC;
-use Buckaroo\PaymentMethods\KlarnaKP\KlarnaKP;
-use Buckaroo\PaymentMethods\KlarnaPay\KlarnaPay;
-use Buckaroo\PaymentMethods\Marketplaces\Marketplaces;
-use Buckaroo\PaymentMethods\Payconiq\Payconiq;
+use Buckaroo\Exceptions\SDKException;
+use Buckaroo\PaymentMethods\iDin\iDin;
+use Buckaroo\PaymentMethods\SEPA\SEPA;
+use Buckaroo\PaymentMethods\iDeal\iDeal;
+use Buckaroo\PaymentMethods\Tinka\Tinka;
+use Buckaroo\Exceptions\BuckarooException;
+use Buckaroo\PaymentMethods\Alipay\Alipay;
 use Buckaroo\PaymentMethods\Paypal\Paypal;
+use Buckaroo\PaymentMethods\Sofort\Sofort;
+use Buckaroo\PaymentMethods\Belfius\Belfius;
+use Buckaroo\PaymentMethods\Billink\Billink;
+use Buckaroo\PaymentMethods\Giropay\Giropay;
+use Buckaroo\PaymentMethods\iDealQR\iDealQR;
+use Buckaroo\PaymentMethods\Surepay\Surepay;
+use Buckaroo\PaymentMethods\Trustly\Trustly;
+use Buckaroo\PaymentMethods\Afterpay\Afterpay;
+use Buckaroo\PaymentMethods\ApplePay\ApplePay;
+use Buckaroo\PaymentMethods\GiftCard\GiftCard;
+use Buckaroo\PaymentMethods\KlarnaKP\KlarnaKP;
+use Buckaroo\PaymentMethods\Payconiq\Payconiq;
+use Buckaroo\PaymentMethods\Emandates\Emandates;
+use Buckaroo\PaymentMethods\KlarnaPay\KlarnaPay;
+use Buckaroo\PaymentMethods\WeChatPay\WeChatPay;
+use Buckaroo\PaymentMethods\Bancontact\Bancontact;
+use Buckaroo\PaymentMethods\CreditCard\CreditCard;
+use Buckaroo\PaymentMethods\Przelewy24\Przelewy24;
+use Buckaroo\PaymentMethods\BuckarooVoucher\Voucher;
+use Buckaroo\PaymentMethods\CreditClick\CreditClick;
 use Buckaroo\PaymentMethods\PayPerEmail\PayPerEmail;
 use Buckaroo\PaymentMethods\PointOfSale\PointOfSale;
-use Buckaroo\PaymentMethods\Przelewy24\Przelewy24;
-use Buckaroo\PaymentMethods\RequestToPay\RequestToPay;
-use Buckaroo\PaymentMethods\SEPA\SEPA;
-use Buckaroo\PaymentMethods\Sofort\Sofort;
-use Buckaroo\PaymentMethods\Subscriptions\Subscriptions;
-use Buckaroo\PaymentMethods\Surepay\Surepay;
-use Buckaroo\PaymentMethods\Tinka\Tinka;
 use Buckaroo\PaymentMethods\BankTransfer\BankTransfer;
-use Buckaroo\PaymentMethods\Trustly\Trustly;
-use Buckaroo\PaymentMethods\WeChatPay\WeChatPay;
-use Buckaroo\Transaction\Client;
+use Buckaroo\PaymentMethods\Marketplaces\Marketplaces;
+use Buckaroo\PaymentMethods\RequestToPay\RequestToPay;
+use Buckaroo\PaymentMethods\Subscriptions\Subscriptions;
+use Buckaroo\PaymentMethods\BuckarooWallet\BuckarooWallet;
+use Buckaroo\PaymentMethods\CreditManagement\CreditManagement;
+use Buckaroo\PaymentMethods\AfterpayDigiAccept\AfterpayDigiAccept;
 
 class PaymentMethodFactory
 {
@@ -104,6 +105,7 @@ class PaymentMethodFactory
         BankTransfer::class             => ['transfer'],
         RequestToPay::class             => ['requesttopay'],
         WeChatPay::class                => ['wechatpay'],
+        Voucher::class                  => ['buckaroovoucher'],
     ];
 
     /**
