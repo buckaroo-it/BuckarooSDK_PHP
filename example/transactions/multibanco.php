dfc52bc7eb7a41f085ca8106a815b5d860c33d94
Ivascu Madalin <madalin.ivascu@arnia.ro>
2023-10-17 10:05:53 +0300

    BP-3020 Add payment method "Multibanco"

---
+++ example/transactions/multibanco.php
@@ -0,0 +1,21 @@
+<?php
+
+require('../bootstrap.php');
+
+use Buckaroo\BuckarooClient;
+
+$buckaroo = new BuckarooClient($_ENV['BPE_WEBSITE_KEY'], $_ENV['BPE_SECRET_KEY']);
+
+//Also accepts json
+//Pay
+$response = $buckaroo->method('multibanco')->pay([
+    'invoice' => uniqid(),
+    'amountDebit' => 10.10,
+]);
+
+//Refund
+$response = $buckaroo->method('multibanco')->refund([
+    'invoice' => '', //Set invoice number of the transaction to refund
+    'originalTransactionKey' => '', //Set transaction key of the transaction to refund
+    'amountCredit' => 10.10,
+]);
--- src/PaymentMethods/PaymentMethodFactory.php
+++ src/PaymentMethods/PaymentMethodFactory.php
@@ -47,6 +47,7 @@ use Buckaroo\PaymentMethods\KBC\KBC;
 use Buckaroo\PaymentMethods\KlarnaKP\KlarnaKP;
 use Buckaroo\PaymentMethods\KlarnaPay\KlarnaPay;
 use Buckaroo\PaymentMethods\Marketplaces\Marketplaces;
+use Buckaroo\PaymentMethods\Multibanco\Multibanco;
 use Buckaroo\PaymentMethods\NoServiceSpecifiedPayment\NoServiceSpecifiedPayment;
 use Buckaroo\PaymentMethods\Payconiq\Payconiq;
 use Buckaroo\PaymentMethods\PaymentInitiation\PaymentInitiation;
@@ -95,6 +96,7 @@ class PaymentMethodFactory
         In3Old::class => ['in3old'],
         KlarnaPay::class => ['klarna', 'klarnain'],
         KlarnaKP::class => ['klarnakp'],
+        Multibanco::class => ['multibanco'],
         Surepay::class => ['surepay'],
         Subscriptions::class => ['subscriptions'],
         SEPA::class => ['sepadirectdebit', 'sepa'],
