96c957b703c39e0b612cbd7c88c293226bef307e
Lucian Turiac <106905746+LucianTuriacArnia@users.noreply.github.com>
2023-11-13 14:39:27 +0200

    Phone number fixes (#144)
    * Remove unused phone parameters
    
    * Remove comma after last element from array

--- src/PaymentMethods/KlarnaKP/Service/ParameterKeys/PhoneAdapter.php
+++ src/PaymentMethods/KlarnaKP/Service/ParameterKeys/PhoneAdapter.php
@@ -25,7 +25,6 @@ namespace Buckaroo\PaymentMethods\KlarnaKP\Service\ParameterKeys;
 class PhoneAdapter extends RecipientAdapter
 {
     protected array $keys = [
-        'landLine' => 'PhoneNumber',
-        'mobile' => 'CellPhoneNumber',
+        'mobile' => 'CellPhoneNumber'
     ];
 }
