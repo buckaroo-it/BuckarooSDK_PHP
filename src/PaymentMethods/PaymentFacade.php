5e9e363b98fe14d7a302e703db5af3f58cee36ad
Shu Chen <shuch3n90@gmail.com>
2024-03-12 07:35:53 +0100

    BA-239 Make it possible to override the version within requests (#168)
    * Able to set service version
    
    * Fix style

--- src/PaymentMethods/PaymentFacade.php
+++ src/PaymentMethods/PaymentFacade.php
@@ -166,8 +166,13 @@ class PaymentFacade
      */
     public function __call(?string $name, array $arguments)
     {
-        if (method_exists($this->paymentMethod, $name))
-        {
+        if (method_exists($this->paymentMethod, $name)) {
+            if($name === 'setServiceVersion') {
+                $this->paymentMethod->setServiceVersion($arguments[0]);
+
+                return $this;
+            }
+
             $this->paymentMethod->setPayload((new PayloadService($arguments[0] ?? []))->toArray());
 
             return $this->paymentMethod->$name();
